c4eadc22e33b2219f24bf05e9abc0d740f4d06f5
Samnangalex06 <samnangalextt@gmail.com>
2026-03-26 11:16:23 +0700

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReceiptsModule } from './receipt/receipt.module'; // adjust if renamed
+import { ReceiptsModule } from './receipt/receipt.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // makes env available everywhere
+      isGlobal: true,
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'sam12345',
+      database: process.env.DB_NAME || 'receipts_db',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
     ReceiptsModule,
   ],
   controllers: [AppController],
